f43a69f26e13d818b692855b55db6190785f6865
Yeon <indesign1106@gmail.com>
2025-06-12 15:19:28 +0900

    리액트 실습 예제

---
+++ 05-redux/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Counter from './components/Counter';
+
+function App() {
+  return (
+    <div>
+      <h1>Redux Toolkit Counter</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
